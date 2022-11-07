38f646ea9aba5f101d334eed3cfb26e0290a24b5
Akash S Panickar <akashsabu@ymail.com>
2022-11-07 08:18:20 +0530

    fix: #1

--- Dream11_login.py
+++ Dream11_login.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+chrome=webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
 chrome.get("https://www.dream11.com")
 try:
     WebDriverWait(chrome, 100).until(EC.visibility_of_element_located((By.XPATH, "//*[@class='matchCardMainTitle_7b586 matchCardMainTitleDesktop_83a22']")))
